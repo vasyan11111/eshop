406078acf40b60c4066eb606f3d4e8e591268555
kohanevich85@gmail.com <d80984704563>
2017-05-27 21:15:55 +0300

    added data source

---
+++ src/main/java/com/eshop/service/DataSource.java
@@ -0,0 +1,42 @@
+package com.eshop.service;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import java.beans.PropertyVetoException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DataSource {
+
+    private static DataSource     datasource;
+    private ComboPooledDataSource cpds;
+
+    private DataSource() throws IOException, SQLException, PropertyVetoException {
+        cpds = new ComboPooledDataSource();
+        cpds.setDriverClass("org.h2.Drive"); //loads the jdbc driver
+        cpds.setJdbcUrl("jdbc:h2:mem:test;INIT=RUNSCRIPT FROM '/sql/create.sql';");
+        cpds.setUser("sa");
+        cpds.setPassword("");
+
+        // the settings below are optional -- c3p0 can work with defaults
+        cpds.setMinPoolSize(5);
+        cpds.setAcquireIncrement(5);
+        cpds.setMaxPoolSize(20);
+        cpds.setMaxStatements(180);
+
+    }
+
+    public static DataSource getInstance() throws IOException, SQLException, PropertyVetoException {
+        if (datasource == null) {
+            datasource = new DataSource();
+            return datasource;
+        } else {
+            return datasource;
+        }
+    }
+
+    public Connection getConnection() throws SQLException {
+        return this.cpds.getConnection();
+    }
+}
