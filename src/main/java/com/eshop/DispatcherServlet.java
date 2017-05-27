28ddb73b6392bf1b1fbb15fb037e65c78246fb0b
kohanevich85@gmail.com <d80984704563>
2017-05-27 19:42:35 +0300

    init commit DispatcherServlet

---
+++ src/main/java/com/eshop/DispatcherServlet.java
@@ -0,0 +1,21 @@
+package com.eshop;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/")
+public class DispatcherServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/pages/homePage.jsp");
+        requestDispatcher.forward(req, resp);
+
+    }
+}
---
+++ src/main/java/com/eshop/Main.java
@@ -0,0 +1,7 @@
+package com.eshop;
+
+/**
+ * Created by deniskohanevich on 5/27/17.
+ */
+public class Main {
+}
